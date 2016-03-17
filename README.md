8dee2e5d010c24ffb00eb107215facb3daf20efd
AlfredXiangWu <alfredxiangwu@gmail.com>
2016-03-17 13:52:41 +0800

    new branch for patch based face image preprocessing

--- README.md
+++
@@ -1,3 +0,0 @@
-# face_preprocessing_cpp
-
-The project is designed for face image pre-processing including face detection(opencv and Shiqi Yu), facial points detection(SDM) and face alignment.
--- inc/data_processing.h
+++
@@ -1,8 +0,0 @@
-#include "face_detection.h"
-#include "face_alignment.h"
-
-
-int perImgProcessing(const Mat img, const string faceDetectModel, const string detectionModelPath, const string trackingModelPath, 
-					 const float ec_mc_y, const float ec_y, vecM &result);
-int batchProcessing(const string rootPath, const string listPath, const string faceDetectModel, const string detectionModelPath, 
-					const string trackingModelPath, const float ec_mc_y, const float ec_y, const string saveRootPath, const int size);
